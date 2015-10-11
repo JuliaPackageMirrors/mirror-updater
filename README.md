07253a88c1ef57c37d62cebdb4f513b20bf470ab
Michael Hatherly <michaelhatherly@gmail.com>
2015-10-11 14:09:05 +0200

    Properly mirror all branchs. Add readme note.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mirror-updater
+
 Update script for JuliaPackageMirrors.
+
+## Removing Mirrors
+
+Package authors may request removal of their packages from this mirror. All genuine requests
+will be accepted without question. Please open an issue,
+https://github.com/JuliaPackageMirrors/mirror-updater/issues, with any package removal
+requests.
--- update.jl
+++ update.jl
@@ -1,14 +1,26 @@
 #!/usr/bin/env julia
 
+module MirrorUpdater
+
+### Blacklisted Packages. ###
+
+const BLACKLIST = [
+
+]
+
 ### Constants. ###
 
 const METADATA   = Pkg.dir("METADATA")
 const ORG_NAME   = "JuliaPackageMirrors"
 const PACKAGES   = readdir(METADATA)
 const TOTAL      = length(PACKAGES)
-const NO_UPDATES = "Already up-to-date.\n"
 
 ### Functions. ###
+
+function rename_url(url)
+    url = replace(url, "git://", "https://")
+    url = replace(url, ".git", "")
+end
 
 function print_header(name, number, width)
     println("[$(lpad(number, width)) / $(lpad(TOTAL, width))] $name")
@@ -21,10 +33,10 @@ function update(name)
         file = joinpath(dir, "url")
         if isfile(file)
             url = strip(readall(file))
-            isdir(name) || run(`git clone $(url) $(name)`)
+            isdir(name) || run(`git clone --mirror $(url) $(name)`)
             cd(name) do
-                # Skip pushing to mirror if there aren't any updates.
-                update_local(url) == NO_UPDATES || update_mirror(name)
+                update_local(url)
+                update_mirror(name, rename_url(url))
             end
         end
     end
@@ -32,36 +44,41 @@ end
 
 function update_local(url)
     try
-        readall(`git pull`)
+        info("fetching from origin: '$url'")
+        run(`git fetch -p origin`)
     catch
-        warn("could not pull from '$(url)'.")
-        # Assume no updates when pulling from origin fails.
-        NO_UPDATES
+        warn("could not fetch from '$(url)'")
     end
 end
 
-function update_mirror(name)
-    try
-        # `--mirror` will push all branches, remotes, and tags to mirror.
-        run(`git push $(ORG_NAME) --mirror`)
-    catch
-        run(`hub create $(ORG_NAME)/$(name).jl`)
-        run(`git remote add $(ORG_NAME) git@github.com:$(ORG_NAME)/$(name).jl.git`)
-        run(`git push $(ORG_NAME) --mirror`)
+function update_mirror(name, url)
+    # Check if mirror exists already.
+    if !success(`git ls-remote git@github.com:$(ORG_NAME)/$(name).jl.git`)
+        info("creating new repo: '$name'")
+        run(`hub create $(ORG_NAME)/$(name).jl -d 'Julia package mirror.' -h $url`)
     end
+    success(`git remote add $(ORG_NAME) git@github.com:$(ORG_NAME)/$(name).jl.git`)
+    # Update the mirrored repo.
+    info("updating mirror: '$name'")
+    success(`git push $(ORG_NAME) --mirror`)
 end
 
 ### Main. ###
 
-# Update METADATA repo first to find newly added packages.
-Pkg.update()
+function main()
+    # Update METADATA repo first to find newly added packages.
+    Pkg.update()
 
-info("updating Julia package mirrors...")
-cd(joinpath(dirname(@__FILE__), "..")) do
-    width = length(digits(length(PACKAGES)))
-    for (number, name) in enumerate(PACKAGES)
-        print_header(name, number, width)
-        update(name)
-        println()
+    info("updating Julia package mirrors...")
+    cd(joinpath(dirname(@__FILE__), "..")) do
+        width = length(digits(length(PACKAGES)))
+        for (number, name) in enumerate(PACKAGES)
+            name in BLACKLIST && (warn("skipping $name"); continue)
+            print_header(name, number, width)
+            update(name)
+            println()
+        end
     end
+end
+
 end
